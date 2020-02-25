71636d89349fc2f0059cd3b495c3feb1cd21fdb7
deshraj5375 <49837875+deshraj5375@users.noreply.github.com>
2020-02-25 16:56:57 +0530

    adding more content

--- PythonScript/dircreation.py
+++ PythonScript/dircreation.py
@@ -1,16 +1,47 @@
 import os
+import sys
+
+"""
+This is to create and remove folder from f0100 to f0150.
+Program will accept input 1 for creating folder from command line. 2 for removing. 
+
+"""
 
 
-def fun_createFolder():
-    dirPrefix='f0'
-    for i in range(100,151):
-        dirname=dirPrefix+str(i)
-        pathtoadd="C:/Users/deshraj/testing/"+dirname
+def fun_createFolder(pathtoadd):
+    try:
         os.mkdir(pathtoadd)
-        print(pathtoadd)
+    except:
+        print("Unable to create dir")
+
+
+def fun_removeFolder(pathtoadd):
+    try:
+        os.rmdir(pathtoadd)
+    except:
+        print("Unable to delete dir")
 
 
 if __name__ == '__main__':
-    fun_createFolder()
+
+    argumentList=sys.argv
+    print(argumentList[1])
+    dirPrefix = 'f0'
+    for i in range(100,151):
+        dirname=dirPrefix+str(i)
+        pathtoadd="C:/Users/CD88TR3/testing/"+dirname
+        if int(argumentList[1]) == 1:
+            fun_createFolder(pathtoadd)
+        elif int(argumentList[1]) == 2:
+            fun_removeFolder(pathtoadd)
+        else:
+            print("Wrong choice")
+
+
+
+
+
+
+
 
 
