6a4ed18025dbaae12279ce61161bc11d9bb8007b
deshraj5375 <49837875+deshraj5375@users.noreply.github.com>
2020-02-26 12:08:56 +0530

    adding file creation

---
+++ PythonScript/filecreation.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+
+def filecheck(path):
+    if os.path.isfile(path):
+        print("file present")
+    else:
+        print("File does not exist ,creating file")
+        filecreate(path)
+
+def filecreate(path):
+    with open(path, 'w'):
+        pass
+
+
+if __name__ == '__main__':
+    filecheck("C:/Users/CD88TR3/testing/test.json")
+
+
+
