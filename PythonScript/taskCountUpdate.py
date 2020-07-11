50ef32b3eed883221210e7e0463c84b49689ef54
deshraj5375 <49837875+deshraj5375@users.noreply.github.com>
2020-07-11 17:47:24 +0530

    Add files via upload

--- PythonScript/taskCountUpdate.py
+++ PythonScript/taskCountUpdate.py
@@ -1,6 +1,7 @@
 import json
+import sys
 
-replaceValue=10
+replaceValue=sys.argv[1]
 f=open("test.json",'r')
 fileData=json.load(f)
 for i in fileData:
@@ -9,4 +10,4 @@ for i in fileData:
     break  
 f.close() 
 with open('test.json', 'w') as f:
-        json.dump(fileData, f)  
+        json.dump(fileData, f)
